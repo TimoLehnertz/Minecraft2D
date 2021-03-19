f8b0dfd9a808ca2634ed98ed4f001ccc80122cbd
TimoLehnertz <timolehnertz1@gmail.com>
2021-03-19 22:56:07 +0100

    minecraft

--- php/view.php
+++ php/view.php
@@ -6,7 +6,7 @@ echo $ip;
 
 $mysqli->select_db("Minecraft");
 
-var_dump(dbInsert("USE Minecraft; INSERT INTO views(ip, region) VALUES (?, ?);", $ip, ip_info($ip)));
+var_dump(dbInsert("INSERT INTO Minecraft.views(ip, region) VALUES (?, ?);", $ip, ip_info($ip)));
 
 // header("location: /index.php");
 
