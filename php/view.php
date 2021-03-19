03e0507cd5d2fb8c1bda2c00b5fb75b04edcfea1
TimoLehnertz <timolehnertz1@gmail.com>
2021-03-19 23:22:36 +0100

    view

--- php/view.php
+++ php/view.php
@@ -2,21 +2,10 @@
 include "../data/dbh.php";
 
 $ip = $_SERVER['REMOTE_ADDR'];
-var_dump($ip);
-var_dump(ip_info($ip));
-
-// $mysqli->select_db("Minecraft");
 
 $info = ip_info($ip);
 
 $loc = $info["country"].", ".$info["state"].", ".$info["city"];
-echo "<br>";
-echo $loc;
-echo "<br>";
-// var_dump(dbExecute("INSERT INTO Minecraft.views(ip, region) VALUES (?, ?);", "ss", $ip, $loc));
-// var_dump(dbInsert("INSERT INTO Minecraft.views(ip, region) VALUES ('testa', 'testb');"));
-
-// header("location: /index.php");
 
 if($stmt = $mysqli->prepare("INSERT INTO Minecraft.views(ip, region) VALUES (?, ?);")){
     if(!$stmt->bind_param("ss", $ip, $loc)){
