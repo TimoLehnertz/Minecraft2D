140b335cd36ee8a5439de48bd209759ff8a1d6e8
TimoLehnertz <timolehnertz1@gmail.com>
2021-03-20 12:00:57 +0100

    device

--- php/view.php
+++ php/view.php
@@ -1,92 +1,95 @@
 <?php
-include "../data/dbh.php";
+echo "test";
+// include "../data/dbh.php";
 
 // error_reporting(E_ALL);
 
-$ip = $_SERVER['REMOTE_ADDR'];
-
-$info = ip_info($ip);
-
-// $userAgent = $_SERVER['HTTP_USER_AGENT'];
-
-// var_dump( $userAgent);
-// echo "test";
-
-$loc = $info["country"].", ".$info["state"].", ".$info["city"];
-
-if($stmt = $mysqli->prepare("INSERT INTO Minecraft.views(ip, region) VALUES (?, ?);")){
-    if(!$stmt->bind_param("ss", $ip, $loc){
-        $stmt->close();
-        echo "binding error";
-    }
-    if($stmt->execute()){
-        echo "succsess";
-    }
-    $stmt->close();
-}
+// $ip = $_SERVER['REMOTE_ADDR'];
 
 
-function ip_info($ip = NULL, $purpose = "location", $deep_detect = TRUE) {
-    $output = NULL;
-    if (filter_var($ip, FILTER_VALIDATE_IP) === FALSE) {
-        $ip = $_SERVER["REMOTE_ADDR"];
-        if ($deep_detect) {
-            if (filter_var(@$_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP))
-                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-            if (filter_var(@$_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP))
-                $ip = $_SERVER['HTTP_CLIENT_IP'];
-        }
-    }
-    $purpose    = str_replace(array("name", "\n", "\t", " ", "-", "_"), NULL, strtolower(trim($purpose)));
-    $support    = array("country", "countrycode", "state", "region", "city", "location", "address");
-    $continents = array(
-        "AF" => "Africa",
-        "AN" => "Antarctica",
-        "AS" => "Asia",
-        "EU" => "Europe",
-        "OC" => "Australia (Oceania)",
-        "NA" => "North America",
-        "SA" => "South America"
-    );
-    if (filter_var($ip, FILTER_VALIDATE_IP) && in_array($purpose, $support)) {
-        $ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip));
-        if (@strlen(trim($ipdat->geoplugin_countryCode)) == 2) {
-            switch ($purpose) {
-                case "location":
-                    $output = array(
-                        "city"           => @$ipdat->geoplugin_city,
-                        "state"          => @$ipdat->geoplugin_regionName,
-                        "country"        => @$ipdat->geoplugin_countryName,
-                        "country_code"   => @$ipdat->geoplugin_countryCode,
-                        "continent"      => @$continents[strtoupper($ipdat->geoplugin_continentCode)],
-                        "continent_code" => @$ipdat->geoplugin_continentCode
-                    );
-                    break;
-                case "address":
-                    $address = array($ipdat->geoplugin_countryName);
-                    if (@strlen($ipdat->geoplugin_regionName) >= 1)
-                        $address[] = $ipdat->geoplugin_regionName;
-                    if (@strlen($ipdat->geoplugin_city) >= 1)
-                        $address[] = $ipdat->geoplugin_city;
-                    $output = implode(", ", array_reverse($address));
-                    break;
-                case "city":
-                    $output = @$ipdat->geoplugin_city;
-                    break;
-                case "state":
-                    $output = @$ipdat->geoplugin_regionName;
-                    break;
-                case "region":
-                    $output = @$ipdat->geoplugin_regionName;
-                    break;
-                case "country":
-                    $output = @$ipdat->geoplugin_countryName;
-                    break;
-                case "countrycode":
-                    $output = @$ipdat->geoplugin_countryCode;
-                    break;
-            }
-        }
-    }
-    return $output;
-}
+
+// $info = ip_info($ip);
+
+// // $userAgent = $_SERVER['HTTP_USER_AGENT'];
+
+// // var_dump( $userAgent);
+// // echo "test";
+
+// $loc = $info["country"].", ".$info["state"].", ".$info["city"];
+
+// if($stmt = $mysqli->prepare("INSERT INTO Minecraft.views(ip, region) VALUES (?, ?);")){
+//     if(!$stmt->bind_param("ss", $ip, $loc){
+//         $stmt->close();
+//         echo "binding error";
+//     }
+//     if($stmt->execute()){
+//         echo "succsess";
+//     }
+//     $stmt->close();
+// }
+
+
+// function ip_info($ip = NULL, $purpose = "location", $deep_detect = TRUE) {
+//     $output = NULL;
+//     if (filter_var($ip, FILTER_VALIDATE_IP) === FALSE) {
+//         $ip = $_SERVER["REMOTE_ADDR"];
+//         if ($deep_detect) {
+//             if (filter_var(@$_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP))
+//                 $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+//             if (filter_var(@$_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP))
+//                 $ip = $_SERVER['HTTP_CLIENT_IP'];
+//         }
+//     }
+//     $purpose    = str_replace(array("name", "\n", "\t", " ", "-", "_"), NULL, strtolower(trim($purpose)));
+//     $support    = array("country", "countrycode", "state", "region", "city", "location", "address");
+//     $continents = array(
+//         "AF" => "Africa",
+//         "AN" => "Antarctica",
+//         "AS" => "Asia",
+//         "EU" => "Europe",
+//         "OC" => "Australia (Oceania)",
+//         "NA" => "North America",
+//         "SA" => "South America"
+//     );
+//     if (filter_var($ip, FILTER_VALIDATE_IP) && in_array($purpose, $support)) {
+//         $ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip));
+//         if (@strlen(trim($ipdat->geoplugin_countryCode)) == 2) {
+//             switch ($purpose) {
+//                 case "location":
+//                     $output = array(
+//                         "city"           => @$ipdat->geoplugin_city,
+//                         "state"          => @$ipdat->geoplugin_regionName,
+//                         "country"        => @$ipdat->geoplugin_countryName,
+//                         "country_code"   => @$ipdat->geoplugin_countryCode,
+//                         "continent"      => @$continents[strtoupper($ipdat->geoplugin_continentCode)],
+//                         "continent_code" => @$ipdat->geoplugin_continentCode
+//                     );
+//                     break;
+//                 case "address":
+//                     $address = array($ipdat->geoplugin_countryName);
+//                     if (@strlen($ipdat->geoplugin_regionName) >= 1)
+//                         $address[] = $ipdat->geoplugin_regionName;
+//                     if (@strlen($ipdat->geoplugin_city) >= 1)
+//                         $address[] = $ipdat->geoplugin_city;
+//                     $output = implode(", ", array_reverse($address));
+//                     break;
+//                 case "city":
+//                     $output = @$ipdat->geoplugin_city;
+//                     break;
+//                 case "state":
+//                     $output = @$ipdat->geoplugin_regionName;
+//                     break;
+//                 case "region":
+//                     $output = @$ipdat->geoplugin_regionName;
+//                     break;
+//                 case "country":
+//                     $output = @$ipdat->geoplugin_countryName;
+//                     break;
+//                 case "countrycode":
+//                     $output = @$ipdat->geoplugin_countryCode;
+//                     break;
+//             }
+//         }
+//     }
+//     return $output;
+// }
