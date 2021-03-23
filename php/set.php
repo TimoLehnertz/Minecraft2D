baf56d101501661f697e854f6d53f54fde337968
Timo Lehnertz <timo.lehnertz@BK.local>
2021-03-23 16:11:44 +0100

    s

--- php/set.php
+++ php/set.php
@@ -1,21 +1,21 @@
-<?php
+<!-- <?php
 
-if(isset($_GET["mouse"])){
-    echo "mouse";
-    file_put_contents('get.html', 'mouse');
-}
+// if(isset($_GET["mouse"])){
+//     echo "mouse";
+//     file_put_contents('get.html', 'mouse');
+// }
 
-if(isset($_GET["clear"])){
-    echo "cleared";
-    file_put_contents('get.html', '');
-}
+// if(isset($_GET["clear"])){
+//     echo "cleared";
+//     file_put_contents('get.html', '');
+// }
 
-if(isset($_GET["set"]) && isset($_GET["value"])){
-    echo "setted ".$_GET["value"];
-    file_put_contents('get.html', $_GET["value"]);
-}
+// if(isset($_GET["set"]) && isset($_GET["value"])){
+//     echo "setted ".$_GET["value"];
+//     file_put_contents('get.html', $_GET["value"]);
+// }
 
-echo "content: ".file_get_contents('get.html');
+// echo "<br>content: ".file_get_contents('get.html');
 ?>
 
 <hr>
@@ -28,4 +28,4 @@ echo "content: ".file_get_contents('get.html');
 <form action="#">
     <input type="text" name="value">
     <button type="submit" name="set">set</button>
-</form>
+</form> -->
